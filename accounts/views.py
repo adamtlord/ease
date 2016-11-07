bd8afefd33cd07c2d18806aabf8f5e762752cc0a
Adam Lord <adam.lord@gmail.com>
2016-11-07 09:46:17 -0800

    Some form cleanup, update customer form working

--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,7 @@ from django.forms import inlineformset_factory
 from accounts.models import CustomerProfile
 from accounts.forms import CustomerProfileForm
 from rides.models import Destination
-from rides.forms import DestinationForm, HomeForm
+from rides.forms import DestinationForm, CreateHomeForm, UpdateHomeForm
 
 
 def customer_list(request, template='accounts/customerprofile_list.html'):
@@ -18,28 +18,35 @@ def customer_list(request, template='accounts/customerprofile_list.html'):
 
 def customer_create(request, template='accounts/customerprofile_create.html'):
 
-    DestinationFormSet = inlineformset_factory(CustomerProfile, Destination, form=DestinationForm)
+    DestinationFormSet = inlineformset_factory(CustomerProfile,
+                                               Destination,
+                                               form=DestinationForm,
+                                               can_delete=False)
 
     if request.method == "POST":
         form = CustomerProfileForm(request.POST)
-        if form.is_valid():
+        home_form = CreateHomeForm(request.POST)
+        if form.is_valid() and home_form.is_valid():
             new_customer = form.save()
+            home_address = home_form.save(commit=False)
+            home_address.customer = new_customer
+            home_address.home = True
+            home_address.save()
 
-            home_form = HomeForm(request.POST)
-            if home_form.is_valid():
-                home_address = home_form.save(commit=False)
-                home_address.customer = new_customer
-                home_address.save()
-
-            destination_formset = DestinationFormSet(request.POST, instance=new_customer)
+            destination_formset = DestinationFormSet(request.POST,
+                                                     instance=new_customer)
             if destination_formset.is_valid():
                 destination_formset.save()
 
-        return redirect('customer_detail', new_customer.id)
+            messages.add_message(request, messages.SUCCESS, 'New customer {} created successfully!'.format(new_customer))
+            return redirect('customer_detail', new_customer.id)
+
+        else:
+            destination_formset = DestinationFormSet(request.POST)
 
     else:
         form = CustomerProfileForm()
-        home_form = HomeForm()
+        home_form = CreateHomeForm()
         destination_formset = DestinationFormSet()
 
     d = {
@@ -58,9 +65,38 @@ def customer_detail(request, pk, template='accounts/customerprofile_detail.html'
     return render(request, template, d)
 
 
-def customer_update(request, pk, template='accounts/customerprofile_form.html'):
-    d = {}
-    d['customer'] = get_object_or_404(CustomerProfile, pk=pk)
+def customer_update(request, pk, template='accounts/customerprofile_update.html'):
+
+    customer = get_object_or_404(CustomerProfile, pk=pk)
+
+    DestinationFormSet = inlineformset_factory(CustomerProfile,
+                                               Destination,
+                                               form=DestinationForm,
+                                               can_delete=True)
+
+    if request.method == "POST":
+        form = CustomerProfileForm(request.POST)
+        home_form = UpdateHomeForm(request.POST, instance=customer.home)
+        destination_formset = DestinationFormSet(request.POST, instance=customer, queryset=Destination.objects.exclude(home=True))
+
+        if all([form.is_valid(), home_form.is_valid(), destination_formset.is_valid()]):
+            new_customer = form.save()
+            home_address = home_form.save()
+            destination_formset.save()
+
+            messages.add_message(request, messages.SUCCESS, 'Customer {} successfully updated!'.format(customer))
+            return redirect('customer_detail', customer.id)
+
+    else:
+        form = CustomerProfileForm(instance=customer)
+        home_form = UpdateHomeForm(instance=customer.home)
+        destination_formset = DestinationFormSet(instance=customer, queryset=Destination.objects.exclude(home=True))
+
+    d = {
+        'form': form,
+        'home_form': home_form,
+        'destination_formset': destination_formset
+    }
 
     return render(request, template, d)
 
--- common/models.py
+++ common/models.py
@@ -6,11 +6,11 @@ from localflavor.us.models import PhoneNumberField, USStateField, USZipCodeField
 
 class Location(models.Model):
     name = models.CharField(max_length=50, blank=True, null=True)
-    street1 = models.CharField(max_length=100, blank=True, null=True)
-    street2 = models.CharField(max_length=100, blank=True, null=True)
+    street1 = models.CharField(max_length=100, blank=True, null=True, verbose_name="Address 1")
+    street2 = models.CharField(max_length=100, blank=True, null=True, verbose_name="Address 2")
     city = models.CharField(max_length=100, blank=True, null=True)
     state = USStateField(blank=True, null=True)
-    zip_code = USZipCodeField(blank=True, null=True)
+    zip_code = USZipCodeField(blank=True, null=True, verbose_name="ZIP")
     country = models.CharField(max_length=100, blank=True, null=True, default="U.S.A.")
     phone = PhoneNumberField(blank=True, null=True)
 
