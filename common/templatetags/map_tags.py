65e4787966d4f11f1dea0896c37ce5746504bf53
Adam Lord <adam.lord@gmail.com>
2016-11-23 15:50:40 -0800

    Add gmaps static key

--- common/templatetags/map_tags.py
+++ common/templatetags/map_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.conf import settings
 from django.utils.html import format_html
 
 register = template.Library()
@@ -6,7 +7,8 @@ register = template.Library()
 
 @register.simple_tag
 def static_map(address):
-    return format_html('<div class="static-map"><img src="https://maps.googleapis.com/maps/api/staticmap?&zoom=14&size=360x270&scale=2&markers=color:0x0346b2|{},{}" class="img-responsive" /></div>',
+    return format_html('<div class="static-map"><img src="https://maps.googleapis.com/maps/api/staticmap?&zoom=14&size=360x270&scale=2&markers=color:0x0346b2|{},{}&key={}" class="img-responsive" /></div>',
                        address.latitude,
-                       address.longitude
+                       address.longitude,
+                       settings.GOOGLE_MAPS_API_KEY
     )
