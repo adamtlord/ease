136e6958487fcd233ac2cb63f0632c2580b22f16
Adam Lord <adam.lord@gmail.com>
2016-12-14 14:45:39 -0800

    Fix failure when stripe customer has no subscription

--- billing/utils.py
+++ billing/utils.py
@@ -13,10 +13,13 @@ def get_stripe_subscription(customer):
         stripe_customer = stripe.Customer.retrieve(customer.subscription_account.stripe_id)
 
         if stripe_customer is not None:
-
-            customer_subscription = stripe_customer.subscriptions.data[0]
-            cache.set(cache_key, customer_subscription, 3600)
-            subscription = customer_subscription
+            customer_subscriptions = stripe_customer.subscriptions
+            if customer_subscriptions.data:
+                customer_subscription = customer_subscriptions.data[0]
+                cache.set(cache_key, customer_subscription, 3600)
+                subscription = customer_subscription
+            else:
+                subscription = None
 
         else:
             subscription = None
