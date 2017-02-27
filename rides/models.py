1d2813931ff88264bda37c14fc6a50a966c86ed9
Adam Lord <adam.lord@gmail.com>
2017-02-27 10:21:26 -0800

    Remove start date and time from ride description.
    It needs to be converted from UTC, but since we’re billing same-day I’ll just remove it.

--- rides/models.py
+++ rides/models.py
@@ -122,7 +122,7 @@ class Ride(models.Model):
             startstreet = self.start.street1
         if self.destination.street1:
             destinationstreet = self.destination.street1
-        return '{} - {} to {}'.format(formats.date_format(self.start_date, "SHORT_DATETIME_FORMAT"), startstreet, destinationstreet)
+        return '{} to {}'.format(startstreet, destinationstreet)
 
     @property
     def total_fees_estimate(self):
