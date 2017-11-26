8cb3c1cb60348a6b16d49c8efd9de0d4173f6b59
Adam Lord <adam.lord@gmail.com>
2017-11-25 16:20:18 -0800

    accelearte test cron

--- accounts/cron.py
+++ accounts/cron.py
@@ -3,7 +3,7 @@ from accounts.helpers import send_test_email
 
 
 class TestCronJob(CronJobBase):
-    RUN_EVERY_MINS = 120
+    RUN_EVERY_MINS = 20
 
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
     code = 'accounts.test_cron_job'
