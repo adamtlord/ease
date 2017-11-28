a647d2d23ec5247b3d85fa75813004acedcebdb8
Adam Lord <adam.lord@gmail.com>
2017-11-28 10:57:45 -0800

    lol oops cron timing

--- accounts/cron.py
+++ accounts/cron.py
@@ -5,7 +5,7 @@ from accounts.helpers import send_new_account_emails
 class NewAccountsCronJob(CronJobBase):
     RUN_AT_TIMES = ['23:55']
 
-    schedule = Schedule(run_every_mins=RUN_AT_TIMES)
+    schedule = Schedule(run_at_times=RUN_AT_TIMES)
     code = 'accounts.new_accounts_cron_job'
 
     def do(self):
