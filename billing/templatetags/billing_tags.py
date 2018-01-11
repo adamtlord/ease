92d9cb127effdf279362cccb78e32f8570962f16
Adam Lord <adam.lord@gmail.com>
2018-01-11 14:31:19 -0800

    add currency filter

--- billing/templatetags/billing_tags.py
+++ billing/templatetags/billing_tags.py
@@ -1,4 +1,5 @@
 import datetime
+import locale
 from django import template
 from django.template.defaultfilters import stringfilter
 
@@ -15,3 +16,15 @@ def cc_display(value):
 @register.filter
 def from_timestamp(value):
     return datetime.datetime.fromtimestamp(value)
+
+
+@register.filter(name='currency')
+def currency(value):
+    if not value:
+        return '--'
+    try:
+        locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+    except:
+        locale.setlocale(locale.LC_ALL, '')
+    loc = locale.localeconv()
+    return locale.currency(value, loc['currency_symbol'], grouping=True)
