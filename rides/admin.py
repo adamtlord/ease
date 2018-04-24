00350732421c75b704b758e0930d74b39bad777f
Adam Lord <adam.lord@gmail.com>
2018-04-24 09:37:25 -0700

    raw id fields in ride admin

--- rides/admin.py
+++ rides/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from rides.models import Destination, Ride, RideConfirmation
 
 
+class RideAdmin(admin.ModelAdmin):
+    raw_id_fields = ("destination", "start")
+
+
 admin.site.register(Destination)
-admin.site.register(Ride)
+admin.site.register(Ride, RideAdmin)
 admin.site.register(RideConfirmation)
